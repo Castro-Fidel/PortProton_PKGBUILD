a5cf09513435dcf0d78680dbc7a995832158292f
Boria138 <boriabloger@protonmail.com>
2024-01-31 14:15:23 +0600

    Deleted --noconfirm for avoid drivers confict and fix curl

--- install.sh
+++ install.sh
@@ -2,26 +2,24 @@
 
 SYS_ID="$(cat /sys/devices/virtual/dmi/id/product_name)"
 
-if [[ ! -e "PKGBUILD" ]]
-then curl https://raw.githubusercontent.com/Castro-Fidel/PortProton_PKGBUILD/main/PKGBUILD
-fi
-
-if [[ ! -e "PKGBUILD_STEAMDECK" ]]
-then curl https://raw.githubusercontent.com/Castro-Fidel/PortProton_PKGBUILD/main/PKGBUILD_STEAMDECK
-fi
-
 echo 'Please provide your sudo password'
 
 # STEAMDECK and STEAMDECK OLED
 if [[ ":Jupiter:Galileo:" =~ :"$SYS_ID": ]]; then
+    if [[ ! -e "PKGBUILD_STEAMDECK" ]]
+    then curl -O -L https://raw.githubusercontent.com/Castro-Fidel/PortProton_PKGBUILD/main/PKGBUILD_STEAMDECK
+    fi
     sudo steamos-readonly disable
     sudo pacman-key --init
     sudo pacman-key --update
     sudo pacman-key --populate
     sudo pacman -S --needed --noconfirm --disable-download-timeout base-devel git
-    makepkg -sirc -p PKGBUILD_STEAMDECK --noconfirm
+    makepkg -sirc -p PKGBUILD_STEAMDECK
     sudo steamos-readonly enable
 else
+    if [[ ! -e "PKGBUILD" ]]
+    then curl -O -L https://raw.githubusercontent.com/Castro-Fidel/PortProton_PKGBUILD/main/PKGBUILD
+    fi
     sudo pacman -S --needed --noconfirm --disable-download-timeout base-devel git
     if command -v gnome-shell ; then
         git clone https://aur.archlinux.org/icoextract.git
@@ -29,5 +27,5 @@ else
         makepkg -sirc --noconfirm
         cd ..
     fi
-    makepkg -sirc --noconfirm
+    makepkg -sirc
 fi
